10056e809bfea8c1fac2c0f8bbaff5c2fa5c0c2b
Charles Reace <cwreace@yahoo.com>
2017-11-05 21:36:57 -0500

    initial commit

---
+++ PDOQuery.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Add a one-stop method for prepared query execution
+ */
+class PDOQuery extends PDO
+{
+    /**
+     * Prepare and execute a query
+     *
+     * @param string $sql
+     * @param Array $data  array(':place_holder_1' => 'value_1[,...])
+     * @return PDOStatement
+     */
+    public function preparedQuery($sql, Array $data=array())
+    {
+        $stmt = $this->prepare($sql);
+        if($stmt == false)
+        {
+            throw new Exception('Prepare failed:'.PHP_EOL.print_r($this-errorInfo()));
+        }
+        if(($result = $stmt->execute($data)) == false)
+        {
+            throw new Exception('Execute failed:'.PHP_EOL.print_r($stmt->errorInfo()));
+        }
+        return $stmt;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# pdoquery
+# PDOQuery
+
 Extension of the PDOStatment class for a one-stop query
+
+## Usage
+
+```php
+require_once 'PDOQuery.php';
+$pdo = new PDOQuery($dns, $user, $pass);
+$stmt = $pdo->preparedQuery(
+    'SELECT * FROM my_table WHERE first_name=:first AND last_name=:last',
+    array(':first' => 'John', ':last' => 'Doe');
+);
+while($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    // . . .
+}
+```
