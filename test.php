92b17462e46a22f204e5568fb2d9456cfc639a7d
Charles Reace <cwreace@yahoo.com>
2017-11-06 22:04:56 -0500

    dry up test a bit and handle exceptions

--- test.php
+++ test.php
@@ -3,23 +3,28 @@
  * From this directory, just run `php test.php`
  */
 require __DIR__.'/PDOQuery.php';
-$pdo = new PDOQuery('sqlite:pdoquery_test.sqlite');
-$pdo->preparedQuery('create table test_table(id integer primary key, some_text text)');
-$pdo->preparedQuery(
+$db = 'pdoquery_test.sqlite';
+$pdo = new PDOQuery("sqlite:$db");
+try {
+    $pdo->preparedQuery('create table test_table(id integer primary key, some_text text)');
+    $pdo->preparedQuery(
     'insert into test_table(some_text) values(:text)',
     array(':text' => 'This is a test.')
 );
-$stmt = $pdo->preparedQuery(
+    $stmt = $pdo->preparedQuery(
     'select * from test_table where some_text = :text',
     array(':text' => 'This is a test.')
 );
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-if($row != false && $row['some_text'] == 'This is a test.')
-{
-    echo 'PASS'.PHP_EOL;
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+    if ($row != false && $row['some_text'] == 'This is a test.') {
+        echo 'PASS'.PHP_EOL;
+    } else {
+        echo 'FAIL'.PHP_EOL;
+    }
+} catch(Exception $e) {
+
+} finally {
+    if (file_exists($db)) {
+        unlink($db);
+    }
 }
-else
-{
-    echo 'FAIL'.PHP_EOL;
-}
-unlink('pdoquery_test.sqlite');
