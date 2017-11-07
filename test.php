6c5583d7eeb1e03751d0df4f907fe27a80d80069
Charles Reace <cwreace@yahoo.com>
2017-11-06 19:25:02 -0500

    add a test

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+require __DIR__.'/PDOQuery.php';
+$pdo = new PDOQuery('sqlite:pdoquery_test.sqlite');
+$pdo->preparedQuery('create table test_table(id integer primary key, some_text text)');
+$pdo->preparedQuery(
+    'insert into test_table(some_text) values(:text)',
+    array(':text' => 'This is a test.')
+);
+$stmt = $pdo->preparedQuery(
+    'select * from test_table where some_text = :text',
+    array(':text' => 'This is a test.')
+);
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+if($row != false && $row['some_text'] == 'This is a test.')
+{
+    echo 'PASS'.PHP_EOL;
+}
+else
+{
+    echo 'FAIL'.PHP_EOL;
+}
+unlink('pdoquery_test.sqlite');
